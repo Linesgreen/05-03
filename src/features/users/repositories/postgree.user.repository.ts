6e839f50e6f84e4019c2ec7fa0f7d346184cb5d4
Vladislav Evg <linesgreen@mail.ru>
2024-02-24 03:03:11 +0300

    add comments, and jsDoc adds sql params for block sql injection

--- src/features/users/repositories/postgree.user.repository.ts
+++ src/features/users/repositories/postgree.user.repository.ts
@@ -15,59 +15,78 @@ export class PostgreeUserRepository {
    * @param newUser : UsersDocument
    */
   async addUser(newUser: User): Promise<User> {
-    const login = newUser.accountData.login;
-    const email = newUser.accountData.email;
-    const password = newUser.accountData.passwordHash;
-    const confirmationCode = newUser.emailConfirmation.confirmationCode;
-    const expirationDate = newUser.emailConfirmation.expirationDate.toISOString();
-    const createdAt = newUser.accountData.createdAt;
-    const isConfirmed = newUser.emailConfirmation.isConfirmed;
+    const { login, email, passwordHash, createdAt } = newUser.accountData;
+    const { confirmationCode, expirationDate, isConfirmed } = newUser.emailConfirmation;
+
     const userId = await this.dataSource.query(
       `INSERT INTO public.users(login, email, "passwordHash", "confirmationCode", "expirationDate","createdAt", "isConfirmed")
-             VALUES ('${login}', '${email}',
-                     '${password}',
-                       '${confirmationCode}', '${expirationDate}', '${createdAt}', ${isConfirmed}) 
+             VALUES ($1, $2, $3, $4, $5, $6, $7) 
               RETURNING id;`,
+      [login, email, passwordHash, confirmationCode, expirationDate.toISOString(), createdAt, isConfirmed],
     );
-    //TODO узнать про это
+
+    // Присваиваем новому пользователю идентификатор, возвращенный из базы данных
     newUser.id = userId[0].id;
+    // Возвращаем нового пользователя
     return newUser;
   }
+  /**
+   * Проверяет существование пользователя по логину или email
+   * @param loginOrEmail - Логин или email пользователя
+   * @returns true, если пользователь существует, иначе false
+   */
   async chekUserIsExist(loginOrEmail: string): Promise<boolean> {
     const chekResult = await this.dataSource.query(
       `SELECT EXISTS(SELECT id FROM public.users
-             WHERE email = '${loginOrEmail}' OR login = '${loginOrEmail}')`,
+             WHERE email = $1 OR login = $2)`,
+      [loginOrEmail, loginOrEmail],
     );
-    console.log(chekResult);
     return chekResult.exists;
   }
 
+  /**
+   * Получает пользователя по логину или email
+   * @param logOrEmail - Логин или email пользователя
+   * @returns Объект пользователя или null, если пользователь не найден
+   */
   async getByLoginOrEmail(logOrEmail: string): Promise<User | null> {
     const user = await this.dataSource.query(
       `SELECT id, login, email, "passwordHash", "confirmationCode", "expirationDate","createdAt", "isConfirmed"
-	           FROM public.users
-	           WHERE email = '${logOrEmail}' OR login = '${logOrEmail}'`,
+             FROM public.users
+             WHERE email = $1 OR login = $2`,
+      [logOrEmail, logOrEmail],
     );
     if (user.length === 0) return null;
-    const objUser = User.fromDbToObject(user[0]);
-    console.log(objUser);
-    return objUser;
+    return User.fromDbToObject(user[0]);
   }
+  /**
+   * Получает пользователя по confirmation code
+   * @param code - Confirmation code
+   * @returns Объект пользователя или null, если пользователь не найден
+   */
   async findByConfCode(code: string): Promise<User | null> {
     const user = await this.dataSource.query(
       `SELECT id, login, email, "passwordHash", "confirmationCode", "expirationDate","createdAt", "isConfirmed"
-	           FROM public.users
-	           WHERE "confirmationCode" = '${code}'`,
+             FROM public.users
+             WHERE "confirmationCode" = $1`,
+      [code],
     );
     if (user.length === 0) return null;
-    const objUser = User.fromDbToObject(user[0]);
-    return objUser;
+    return User.fromDbToObject(user[0]);
   }
+  /**
+   * Обновляет поле в таблице users
+   * @param searchField - Поле для поиска
+   * @param searchValue - Значение для поиска
+   * @param field - Поле для обновления
+   * @param value - Новое значение
+   */
   async updateField(searchField: string, searchValue: string, field: string, value: unknown): Promise<void> {
     await this.dataSource.query(
       `UPDATE public.users
-             SET "${field}" = '${value}'
-             WHERE "${searchField}" = '${searchValue}'`,
+             SET "${field}" = $1
+             WHERE "${searchField}" = $2`,
+      [value, searchValue],
     );
   }
 }
